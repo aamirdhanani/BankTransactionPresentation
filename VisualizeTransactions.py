2d12ea258d49e5b503205e9d42b90d7166969156
aa327 <59127865+aamirdhanani@users.noreply.github.com>
2021-05-03 22:07:22 -0400

    Update VisualizeTransactions.py

--- VisualizeTransactions.py
+++ VisualizeTransactions.py
@@ -2,13 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-data = pd.read_csv ('statement.csv')
-df = pd.DataFrame(data, columns=["Amount"]) #Can be changed to work with your bank statements
+data = pd.read_csv ('statement.csv') #Read a CSV file from a specifed location
+df = pd.DataFrame(data, columns=["Amount"]) #Can be changed to work with your bank statements (mine used "Amount" for... well... the transaction amount
 
 amounts =[]
 transnum = []
-
-
 
 for i, j in df.iterrows(): 
     amounts.append(j['Amount'])
